5a89590944d7ef0441cd56f451a423f7e3fdb695
Oliver Strong <oliver@pop-os.localdomain>
2021-07-11 22:49:59 +1000

    nearly got common_voice_single_word importing into audioengine label format

--- audioengine/_nbdev.py
+++ audioengine/_nbdev.py
@@ -2,9 +2,26 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"get_num_gpus": "00_utils.gpu.ipynb",
+         "set_gpu_memory_limit": "00_utils.gpu.ipynb",
+         "set_gpu_list_memory_limit": "00_utils.gpu.ipynb",
+         "recursive_get_size": "00_utils.misc.ipynb",
+         "log_init": "00_utils.misc.ipynb",
+         "log_critical": "00_utils.misc.ipynb",
+         "log_error": "00_utils.misc.ipynb",
+         "log_warning": "00_utils.misc.ipynb",
+         "log_info": "00_utils.misc.ipynb",
+         "log_debug": "00_utils.misc.ipynb",
+         "verify_audioengine_dataset": "00_utils.schema.ipynb",
+         "audioengine_dataset_schema": "00_utils.schema.ipynb",
+         "read_tsv": "utils.imports.common_voice.ipynb",
+         "delete_pandas_columns": "utils.imports.common_voice.ipynb"}
 
-modules = []
+modules = ["utils/gpu.py",
+           "utils/misc.py",
+           "utils/schema.py",
+           "audio_classification.py",
+           "utils/imports/common_voice.py"]
 
 doc_url = "https://LizardLad.github.io/audioengine/"
 
